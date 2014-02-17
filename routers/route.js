2e009e59cb81d32d15e46b658323854245bb4faf
Roman Timashev <roman@tmshv.ru>
2014-02-17 22:45:51 +0400

    = fix: route.js copied from archive;

--- routers/route.js
+++ routers/route.js
@@ -47,6 +47,97 @@ const DOCUMENTS = {
 	}
 };
 
+const TEAM = [
+	{
+		name: "Марина Геннадьевна Тимашева",
+		position: "Директор",
+		edu: "Ленинградский инженерно-строительный институт",
+		diplom: "НВ №222567",
+		status: "",
+		picture: "/img/team/teacher5.jpg"
+	},
+	{
+		name: "Валерий Александрович Саржин",
+		position: "Преподаватель живописи",
+		edu: "Академия художеств имени И. Е. Репина",
+		diplom: "B-I №259798",
+		status: "Первая квалификационная категория по живописи и станковой композиции",
+		picture: "/img/team/teacher10.jpg"
+	},
+	{
+		name: "Ольга Дмитриевна Гоголева",
+		position: "Преподаватель живописи",
+		edu: "Ленинградский инженерно-строительный институт",
+		diplom: "B-I №222572",
+		status: "Высшая квалификационная категория по живописи и станковой композиции",
+		picture: "/img/team/teacher11.jpg"
+	},
+	{
+		name: "Наталья Викторовна Андреева",
+		position: "Преподаватель композиции",
+		edu: "Санкт-Петербургский государственный университет технологии и дизайна",
+		diplom: "ИВС 0514253",
+		status: "Вторая квалификационная категория по прикладной композиции",
+		picture: "/img/team/teacher2.jpg"
+	},
+	{
+		name: "Вера Викторовна Воронова",
+		position: "Преподаватель композииции",
+		edu: "Российский государственный университет имени А. И. Герцена",
+		diplom: "ВСГ 0530340",
+		status: "Вторая квалификационная категория по прикладной композиции",
+		picture: "/img/team/teacher1.jpg"
+	},
+	{
+		name: "Ирина Николаевна Втюрина",
+		position: "Преподаватель скульптуры",
+		edu: "Кировский государственный педагогический институт имени В. И. Ленина",
+		diplom: "ШВ №164501",
+		status: "Первая квалификационная категория по скульптуре",
+		picture: "/img/team/teacher8.jpg"
+	},
+	{
+		name: "Мария Юрьевна Валькова",
+		position: "Преподаватель истории искусства",
+		edu: "Педагогический институт имени А. И. Герцена",
+		diplom: "ПП №224197",
+		status: "Высшая квалификационная категория по истории искусства",
+		picture: "/img/team/teacher.jpg"
+	},
+	{
+		name: "Руслан Камилевич Тимашев",
+		position: "Преподаватель компьютерной графики",
+		edu: "Санкт-Петербургский государственный архитектурно-строительный университет",
+		diplom: "ВСГ 5344910",
+		status: "",
+		picture: "/img/team/teacher4.jpg"
+	},
+	{
+		name: "Анна Сергеевна Манцева",
+		position: "Преподаватель рисунка",
+		edu: "Московский архитектурный институт",
+		diplom: "РН 70241",
+		status: "",
+		picture: "/img/team/teacher3.jpg"
+	},
+	{
+		name: "Серафима Александровна Латыпова",
+		position: "Преподаватель рисунка",
+		edu: "Незаконченный Ленинградский государственный педагогический университет имени А. С. Пушкина",
+		diplom: "",
+		status: "",
+		picture: "/img/team/teacher9.jpg"
+	},
+	{
+		name: "Евгения Юрьевна Тарасова",
+		position: "Преподаватель гравюры",
+		edu: "Санкт-Петербургский государственный архитектурно-строительный университет",
+		diplom: "ВСВ 1321444",
+		status: "",
+		picture: "/img/team/teacher.jpg"
+	},
+];
+
 function getMenu(){
 	var menu = {
 		items:[
@@ -61,9 +152,14 @@ function getMenu(){
 				class:"selected green"
 			},
 			{
-				text:"Преподаватели",
-				// url:"/teachers",
+				text:"Занятия",
+				// url:"/schedule",
 				class:"selected"
+			},
+			{
+				text:"Коллектив",
+				url:"/team",
+				class:"selected pink"
 			},
 			{
 				text:"Выпускники",
@@ -132,3 +228,23 @@ exports.menu = function(url){
 }
 
 exports.doc = getDoc;
+exports.team = function(processor) {
+	var list = [];
+	TEAM.forEach(function(m){
+		if(processor){
+			processor(m);
+		}
+		list.push(m);
+	});
+	return list;
+}
+
+exports.nameProcessor = function(delimiter){
+	if(!delimiter) delimiter = /\s+/;
+	return function(member){
+		var nnn = member.name.split(delimiter);
+		member.firstName = nnn[0];
+		member.middleName = nnn[1];
+		member.lastName = nnn[2];		
+	}
+}
