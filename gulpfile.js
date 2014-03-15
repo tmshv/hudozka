00f7f611ff91a26358f6cf9eeb38f6bd6f2d3734
Roman Timashev <roman@tmshv.ru>
2014-03-15 14:39:40 +0400

    + schedule.scss; + animate.min.css; = view/*.scss -> assets/style; = gulpfile.js changed sass task for new env;

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var minifyCSS = require("gulp-minify-css");
+var autoprefixer = require("gulp-autoprefixer");
 var imageMin = require("gulp-imagemin");
 
 var pub = "./public/";
 
 gulp.task("sass", function () {
-    gulp.src("./view/*.scss")
+    gulp.src("./assets/style/*.scss")
         .pipe(sass())
-        // .pipe(minifyCSS())
+        .pipe(minifyCSS())
+        .pipe(autoprefixer())
         .pipe(gulp.dest(pub+"s"));
 });
 
