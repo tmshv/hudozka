bc03d026e739b3ead47402f962ccfc93245bc703
Roman Timashev <roman@tmshv.ru>
2014-03-18 23:19:27 +0400

    = gulpfile + uglify;

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+var path = require("path");
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var minifyCSS = require("gulp-minify-css");
 var autoprefixer = require("gulp-autoprefixer");
 var imageMin = require("gulp-imagemin");
+var uglify = require("gulp-uglify");
 
 var pub = "./public/";
 
@@ -11,21 +13,28 @@ gulp.task("sass", function () {
         .pipe(sass())
         .pipe(minifyCSS())
         .pipe(autoprefixer())
-        .pipe(gulp.dest(pub+"s"));
+        .pipe(gulp.dest("./public/s"));
 });
 
 gulp.task("copy", function(){
 	gulp.src("./assets/fonts/*")
-		.pipe(gulp.dest(pub+"fonts"));
+		.pipe(gulp.dest("./public/fonts"));
+
 	gulp.src("./assets/style/*.css")
-		.pipe(gulp.dest(pub+"s"));
+		.pipe(gulp.dest("./public/s"));
+});
+
+gulp.task("uglify", function(){
+	gulp.src("./client/*.js")
+		.pipe(uglify())
+		.pipe(gulp.dest("./public/c"));
 });
 
 gulp.task("imagemin", function(){
 	gulp.src("./assets/img/**/*")
 		.pipe(imageMin())
-		.pipe(gulp.dest(pub+"img"));
+		.pipe(gulp.dest("./public/img"));
 });
 
-gulp.task("default", ["sass", "copy"]);
+gulp.task("default", ["sass", "copy", "uglify"]);
 gulp.task("deploy", ["default", "imagemin"]);
