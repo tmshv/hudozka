85ca132c3e0cd9c835b47616ba38dd3cf1ca0b2b
Roman Timashev <roman@tmshv.ru>
2015-03-01 18:23:46 +0300

    document title;

--- client/document.js
+++ client/document.js
@@ -4,14 +4,27 @@ var app = angular.module("hudozhka.document", [
     "angulartics.google.analytics"
 ]);
 
-app.controller("DocumentController", function ($scope, $location, docs) {
-    $scope.pageClass = "page-document";
-    $scope.showControls = false;
-
-    var abs = $location.absUrl();
-    var doc_uri = /\/([\w-]+)$/.exec(abs);
-
-    if(doc_uri.length >= 2) {
-        $scope.document = docs.doc(doc_uri[1]);
+app.factory("docname", function ($location) {
+    return function(){
+        var abs = $location.absUrl();
+        var doc_uri = /\/([\w-]+)$/.exec(abs);
+        if(doc_uri.length >= 2) {
+            return doc_uri[1];
+        }else{
+            return null;
+        }
     }
 });
+
+app.controller("DocumentController", function ($rootScope, $scope, docs, docname) {
+    $scope.pageClass = "page-document";
+
+    var n = docname();
+    if(n) {
+        var d = docs.doc(n);
+        $scope.document = d;
+        $rootScope.title = d.title;
+    }
+});
+
+require("./filters/uppercase-first")(app);
