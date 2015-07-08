a9660305068802ea75bc1cf6c9e03684a9c53d9d
Roman Timashev <roman@tmshv.ru>
2015-07-08 20:30:29 +0300

    home page tape viwer init update;

--- client/pages/home.js
+++ client/pages/home.js
@@ -4,8 +4,26 @@
 
 module.exports = function (app) {
     app.controller("HomePageController", function ($scope, config) {
+        var fotorama;
+
         $scope.pageClass = "page-home";
 
-        $(".fotorama").fotorama();
+        var $tape = $('.tape-block');
+        var $fotorama = $('.fotorama');
+
+        $tape.addClass('simplified');
+        $fotorama.on('fotorama:ready', function () {
+            $tape.removeClass('simplified');
+            fotorama.setOptions({
+                nav: 'dots'
+            });
+        });
+
+        fotorama = $fotorama.fotorama().data('fotorama');
+        fotorama.load([
+            {img: 'http://static.shburg.org/art/img/cat1.jpg'},
+            {img: 'http://static.shburg.org/art/img/cat2.jpg'},
+            {img: 'http://static.shburg.org/art/img/cat3.jpg'}
+        ]);
     });
 };
