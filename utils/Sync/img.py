5b6b09b66b2751794b05a1bc46054bdc4b85f070
Roman Timashev <roman@tmshv.ru>
2016-01-16 23:07:42 +0300

    + image magick tools to img.py

--- utils/Sync/img.py
+++ utils/Sync/img.py
@@ -1,5 +1,7 @@
 import os
 
+from subprocess import call
+
 __author__ = 'Roman Timashev'
 
 from PIL import Image
@@ -23,3 +25,8 @@ def override_extension(dest, ext=None):
         dest = os.path.splitext(dest)[0] + ext
 
     return dest
+
+
+def image_magick_resize(input_file, output_file, size, quality=85):
+    s = '%dx%d' % size
+    call(['convert', input_file, '-resize', s, '-quality', str(quality), output_file])
