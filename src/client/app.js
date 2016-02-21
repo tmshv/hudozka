a4d54f386d5d277492c08d2624edf206f8221334
Roman Timashev <roman@tmshv.ru>
2016-02-22 00:56:47 +0300

    app.js ES6 refactoring

--- src/client/app.js
+++ src/client/app.js
@@ -8,112 +8,112 @@ import GalleryItem from './components/GalleryItem';
 import TeamMemberProfile from './components/TeamMemberProfile';
 import api from './api/api';
 
-var deps = [
+let deps = [
     require('./modules/schedule')
 ].map(m => m.name);
 
-var app = angular.module("hudozhka", deps.concat([
-    "hudozhka.data",
-    "ngRoute",
-    "angulartics",
-    "angulartics.google.analytics",
-    "angularSpinner"
+let app = angular.module('hudozhka', deps.concat([
+    'hudozhka.data',
+    'ngRoute',
+    'angulartics',
+    'angulartics.google.analytics',
+    'angularSpinner'
 ]));
 
-app.config(function ($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix("!");
+app.config(($locationProvider, $routeProvider) => {
+    $locationProvider.hashPrefix('!');
     $locationProvider.html5Mode(true);
 
-    var routes = [
+    let routes = [
         {
-            name: "/",
-            templateUrl: "/views/home.html",
-            controller: "HomePageController",
-            title: "ДХШ Шлиссельбурга"
+            name: '/',
+            templateUrl: '/views/home.html',
+            controller: 'HomePageController',
+            title: 'ДХШ Шлиссельбурга'
         },
         {
-            name: "/schedule/:period?/:semester?",
-            templateUrl: "/views/schedule.html",
-            controller: "SchedulePageController",
-            title: "Расписание"
+            name: '/schedule/:period?/:semester?',
+            templateUrl: '/views/schedule.html',
+            controller: 'SchedulePageController',
+            title: 'Расписание'
         },
         {
-            name: "/team",
-            templateUrl: "/views/team.html",
-            controller: "TeamPageController",
-            title: "Преподаватели"
+            name: '/team',
+            templateUrl: '/views/team.html',
+            controller: 'TeamPageController',
+            title: 'Преподаватели'
         },
         {
-            name: "/gallery",
-            templateUrl: "/views/gallery.html",
-            controller: "GalleryPageController",
-            title: "Работы учащихся"
+            name: '/gallery',
+            templateUrl: '/views/gallery.html',
+            controller: 'GalleryPageController',
+            title: 'Работы учащихся'
         },
         {
-            name: "/gallery/:year/:course/:album",
-            templateUrl: "/views/gallery-album.html",
-            controller: "AlbumPageController"
+            name: '/gallery/:year/:course/:album',
+            templateUrl: '/views/gallery-album.html',
+            controller: 'AlbumPageController'
         },
         {
-            name: "/docs",
-            templateUrl: "/views/docs.html",
-            controller: "DocsPageController",
-            title: "Документы"
+            name: '/docs',
+            templateUrl: '/views/docs.html',
+            controller: 'DocsPageController',
+            title: 'Документы'
         }
     ];
 
-    routes.forEach(function (route) {
+    routes.forEach(route => {
         if (typeof route === 'function') route = route();
         $routeProvider.when(route.name, route);
     });
 
     $routeProvider.otherwise({
-        templateUrl: "/404.html"
+        templateUrl: '/404.html'
     });
 });
 
-app.run(function ($location, $rootScope, $http) {
-    $rootScope.$on("$routeChangeSuccess", function (event, current) {
-        var title = current["$$route"].title;
+app.run(($location, $rootScope, $http) => {
+    $rootScope.$on('$routeChangeSuccess', (event, current) => {
+        let title = current['$$route'].title;
         if (title) $rootScope.title = title;
     });
 
-    $http.defaults.headers.common["Accept"] = "application/json";
+    $http.defaults.headers.common['Accept'] = 'application/json';
 });
 
 [
     api,
-    require("./services/io"),
-    require("./directives/mainMenu"),
-    require("./directives/backgroundPicture"),
-    require("./ui/menu"),
-    require("./ui/breadcrumbs"),
-    require("./ui/timeline"),
-    require("./filters/strip"),
-    require("./filters/remove-hashtags"),
-    require("./filters/removeNewline"),
-    require("./filters/uppercase-first"),
-    require("./pages/home"),
-    require("./pages/schedule"),
-    require("./pages/team"),
-    require("./pages/gallery"),
-    require("./pages/docs"),
-    require("./controllers/ContactsController"),
-    require("./controllers/CopyrightController"),
+    require('./services/io'),
+    require('./directives/mainMenu'),
+    require('./directives/backgroundPicture'),
+    require('./ui/menu'),
+    require('./ui/breadcrumbs'),
+    require('./ui/timeline'),
+    require('./filters/strip'),
+    require('./filters/remove-hashtags'),
+    require('./filters/removeNewline'),
+    require('./filters/uppercase-first'),
+    require('./pages/home'),
+    require('./pages/schedule'),
+    require('./pages/team'),
+    require('./pages/gallery'),
+    require('./pages/docs'),
+    require('./controllers/ContactsController'),
+    require('./controllers/CopyrightController'),
     Document,
     Award,
     GalleryItem,
     TeamMemberProfile,
     function (app) {
-        app.controller("SchedulePageController", function ($scope) {
-            $scope.pageClass = "page-schedule";
+        app.controller('SchedulePageController', ($scope) => {
+            $scope.pageClass = 'page-schedule';
         });
 
-        app.controller("DocsPageController", function ($scope) {
-            $scope.pageClass = "page-docs";
+        app.controller('DocsPageController', ($scope) => {
+            $scope.pageClass = 'page-docs';
         });
 
-        app.controller("AppController", function ($scope, menu) {
+        app.controller('AppController', ($scope, menu) => {
             $scope.menuProvider = menu.items;
         });
     }
