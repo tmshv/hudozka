4ac5ef507f2d829fc90dd0b5df87d913cf1d3243
Roman Timashev <roman@tmshv.ru>
2016-02-24 20:54:02 +0300

    net.js, date.js utils ES6

--- src/utils/date.js
+++ src/utils/date.js
@@ -6,12 +6,16 @@ import {range} from './common';
  * @returns {*}
  */
 export function getDates(start) {
-    var day = start.getDay();
-    return range(0, 6, 1).reduce(function (dates, weekDay) {
-        var d = 1 + weekDay - day;
-        var time = start.getTime() + (1000 * 60 * 60 * 24) * d;
-        var date = new Date(time);
-        dates.push(date.getDate());
-        return dates;
-    }, []);
+    const oneDay = (1000 * 60 * 60 * 24);
+    const startTime = start.getTime();
+    const startDay = start.getDay();
+
+    return range(0, 6, 1)
+        .reduce((dates, weekDay) => {
+            let delta = 1 + weekDay - startDay;
+            let time = startTime + (oneDay * delta);
+
+            return dates.concat([new Date(time)]);
+        }, [])
+        .map(i => i.getDate());
 }
--- src/utils/net.js
+++ src/utils/net.js
@@ -1,13 +1,8 @@
-/**
- * Created by tmshv on 08/03/15.
- */
-
 export function query(list) {
-    var params = list.reduce(function (query, param) {
-            var q = param[0] + "=" + param[1];
-            query.push(q);
-            return query;
-        }, [])
-        .join("&");
-    return params
+    return list
+        .reduce(
+            (query, param) => query.concat([`${param[0]}=${param[1]}`]),
+            []
+        )
+        .join('&');
 }
