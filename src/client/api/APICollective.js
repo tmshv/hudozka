f40f164e4d24a217ce812937c1e40dbf700aec3b
Roman Timashev <roman@tmshv.ru>
2016-02-24 20:56:45 +0300

    APICollective sort option

--- src/client/api/APICollective.js
+++ src/client/api/APICollective.js
@@ -1,8 +1,9 @@
 import EndPoint from './EndPoint';
 
 export default class APICollective extends EndPoint {
-    collective() {
+    collective(sort=null) {
         let url = '/collective';
+        if(sort) url = `/collective?sort=${sort.join(',')}`;
         return this.request.get(url, {cache: true});
     }
 }
