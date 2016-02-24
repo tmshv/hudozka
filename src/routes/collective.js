5244beecf8117ce0be73cf2a42b3aa660b32931d
Roman Timashev <roman@tmshv.ru>
2016-02-24 18:02:09 +0300

    + collective route

---
+++ src/routes/collective.js
@@ -0,0 +1,23 @@
+import route from 'koa-route';
+import {json} from './';
+import db from '../core/db';
+import {getCourseNameByID} from '../models/course';
+import team from '../models/team';
+
+export default function (app) {
+    app.use(
+        route.get('/collective', json(
+            function *() {
+                this.type = 'application/json';
+
+                let data = yield db.c('collective').find();
+                if (!data) {
+                    this.status = 404;
+                    return;
+                }
+
+                this.body = data;
+            }
+        ))
+    );
+};
