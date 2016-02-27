fee8ab9c7a3e560fb6c5fa8cb43c667803c44a04
Roman Timashev <roman@tmshv.ru>
2016-02-27 16:16:37 +0300

    tape-viewer disabled

--- src/client/components/PageSchool.js
+++ src/client/components/PageSchool.js
@@ -6,32 +6,32 @@ export default function (app) {
         controller: function() {
             this.pageClass = 'page-school';
 
-            let fotorama;
-            let $tape = $('.tape-block');
-            let $fotorama = $tape.find('.tape-block__content');
-
-            $tape.addClass('simplified');
-            $fotorama.on('fotorama:ready', () => {
-                $tape.removeClass('simplified');
-                fotorama.setOptions({
-                    nav: 'dots',
-                    transition: 'crossfade',
-                    autoplay: '6000',
-                    arrows: 'false',
-                    width: '100%',
-                    fit: 'cover',
-                    loop: 'true'
-                });
-            });
-
-            fotorama = $fotorama.fotorama().data('fotorama');
-            setTimeout(() => {
-                fotorama.load([
-                    {img: 'https://static.shburg.org/art/img/cat1.jpg'},
-                    {img: 'https://static.shburg.org/art/img/cat2.jpg'},
-                    {img: 'https://static.shburg.org/art/img/cat3.jpg'}
-                ]);
-            }, 100);
+            //let fotorama;
+            //let $tape = $('.tape-block');
+            //let $fotorama = $tape.find('.tape-block__content');
+            //
+            //$tape.addClass('simplified');
+            //$fotorama.on('fotorama:ready', () => {
+            //    $tape.removeClass('simplified');
+            //    fotorama.setOptions({
+            //        nav: 'dots',
+            //        transition: 'crossfade',
+            //        autoplay: '6000',
+            //        arrows: 'false',
+            //        width: '100%',
+            //        fit: 'cover',
+            //        loop: 'true'
+            //    });
+            //});
+            //
+            //fotorama = $fotorama.fotorama().data('fotorama');
+            //setTimeout(() => {
+            //    fotorama.load([
+            //        {img: 'https://static.shburg.org/art/img/cat1.jpg'},
+            //        {img: 'https://static.shburg.org/art/img/cat2.jpg'},
+            //        {img: 'https://static.shburg.org/art/img/cat3.jpg'}
+            //    ]);
+            //}, 100);
         }
     });
 };
