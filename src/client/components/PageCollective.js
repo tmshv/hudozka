16a76fd7939f57df2fe513550d4bb65207380ce2
Roman Timashev <roman@tmshv.ru>
2016-02-27 16:16:57 +0300

    + commented code

--- src/client/components/PageCollective.js
+++ src/client/components/PageCollective.js
@@ -11,6 +11,31 @@ export default function (app) {
                 .success(collective => {
                     this.members = collective;
                 });
+
+            //let rangelen = r => r[1] - r[0];
+            //let interpolate = (r, i) => (i - r[0]) / rangelen(r);
+            //let extrapolate = (r, i) => r[0] + rangelen(r) * i;
+            //
+            //let mapValue = (from, to) =>
+            //    i => extrapolate(
+            //        to, interpolate(
+            //            from, i
+            //        ));
+            //
+            //let mapImageScroll = mapValue([0, 400], [0, -100]);
+            //
+            //$(window).scroll(() => {
+            //    let scroll = $(window).scrollTop();
+            //
+            //    let v = mapImageScroll(scroll);
+            //    console.log(v);
+            //
+            //    this.$apply(() => {
+            //        this.imageStyle = {
+            //            top: v
+            //        };
+            //    });
+            //});
         }
     });
 };
