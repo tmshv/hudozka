47a077e0c1dae4c4e5ca8a4f508cae898cd5bea2
Roman Timashev <roman@tmshv.ru>
2016-02-27 19:40:25 +0300

    documents categories

--- src/client/components/PageDocuments.js
+++ src/client/components/PageDocuments.js
@@ -1,14 +1,34 @@
 import template from '../../templates/components/page-documents.html';
 
+let unique = fn => list => [...new Set(list.map(fn))];
+
 export default function (app) {
     app.component('pageDocuments', {
         template: template,
         controller: function (api) {
             this.pageClass = 'page-documents';
+            let categories = unique(i => i.category);
+
+            this.collections = [];
 
             api.document.documents()
-                .success(data => {
-                    this.documents = data;
+                .success(documents => {
+                    let cats = categories(documents)
+                        .map(cat => new Object({
+                            name: cat,
+                            documents: []
+                        }));
+
+                    let docCollections = documents.reduce(
+                        (cats, doc) => {
+                            let c = cats.find(i => i.name === doc.category);
+                            c.documents.push(doc);
+                            return cats;
+                        },
+                        cats
+                    );
+
+                    this.collections = this.collections.concat(docCollections);
                 });
 
             api.document.awards()
