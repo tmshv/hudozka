fa76f1976236b8e8df13c8f872ab3368f88d4340
Roman Timashev <roman@tmshv.ru>
2016-03-18 05:50:48 +0300

    - instagram features

--- src/bin/www.js
+++ src/bin/www.js
@@ -4,16 +4,8 @@ require('babel-polyfill');
 var config = require("../config").default;
 var db = require("../core/db");
 var app = require("../server").app;
-var instagram = require("../instagram");
 
 db.init(config.db.uri)
-    //.then(instagram)
     .then(function () {
         app.listen(config.port);
-    })
-    .then(function () {
-        return instagram()
-            .then(function () {
-                console.log('INSTAGRAM INITED');
-            });
     });
