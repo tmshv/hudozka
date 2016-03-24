70707a22442e4db5d140bb005103ddd802095298
Roman Timashev <roman@tmshv.ru>
2016-03-24 23:51:06 +0300

    Data class + save options

--- src/data/Data.js
+++ src/data/Data.js
@@ -4,11 +4,20 @@ import {collection} from '../core/db';
 export default class Data extends EventEmitter {
     constructor(storeName) {
         super();
+        this.save = true;
         this.name = storeName;
         this.store = collection(storeName);
     }
     
     update(document){
         this.emit('update', document);
+    }
+    
+    disableSaving(){
+        this.save = false;
+    }
+
+    enableSaving(){
+        this.save = true;
     }
 }
