1e03cd012a33b1d4d026bbba3f2fa666d13e4b13
Roman Timashev <roman@tmshv.ru>
2016-03-26 01:45:15 +0300

    + Instagram Service EndPoint request method

--- services/Instagram/src/instagram/EndPoint.js
+++ services/Instagram/src/instagram/EndPoint.js
@@ -1,9 +1,25 @@
+import  request from 'request-promise';
+
 export default class EndPoint{
-    constructor(client) {
+    constructor(client, token) {
         this.client = client;
+        this.token = token;
     }
     
     method(name){
         return this.client[name];
+    }
+
+    request(method, path, data=null) {
+        let buildPath = path => `https://api.instagram.com/v1${path}`;
+        let options = {
+            method: method,
+            uri: buildPath(path),
+            headers: {}
+        };
+        
+        if(data) options.body = data;
+        return request(options)
+            .then(i => JSON.parse(i));
     }
 }
