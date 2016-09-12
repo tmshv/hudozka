b5fdb0d6d6364f1246af6488aeb737636dca3f98
Roman Timashev <roman@tmshv.ru>
2016-09-13 00:15:06 +0300

    [sync] sync.py updated

--- utils/Sync/sync.py
+++ utils/Sync/sync.py
@@ -1,4 +1,5 @@
 import settings
+from db import db
 from sync.data.fs import FSProvider
 from sync.data.yandexdisk import YDProvider
 from sync.document import sync_documents
@@ -13,17 +14,19 @@ def get_provider(provider_type, root):
     return None
 
 
-def main(provider, fn, name):
+def main(provider, fn, collection_name):
     if provider:
-        u, d = fn(provider, update=settings.do_update, delete=settings.do_update)
-        print('DELETE %s: %s' % (name.upper(), 'NO' if not settings.do_delete else str(len(d))))
+        collection = db()[collection_name]
+
+        u, d = fn(provider, collection, update=settings.do_update, delete=settings.do_update)
+        print('DELETE %s: %s' % (collection_name.upper(), 'NO' if not settings.do_delete else str(len(d))))
         lprint_json(d)
 
-        print('UPDATE %s: %s' % (name.upper(), 'NO' if not settings.do_update else str(len(u))))
+        print('UPDATE %s: %s' % (collection_name.upper(), 'NO' if not settings.do_update else str(len(u))))
         lprint_json(u)
 
 
 if __name__ == '__main__':
     client = get_provider(settings.sync_provider_type, settings.dir_documents)
 
-    main(client, sync_documents, 'documents')
+    main(client, sync_documents, settings.collection_documents)
