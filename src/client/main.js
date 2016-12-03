9443f6819559d508c255844f512d27e178a7aba2
Roman Timashev <roman@tmshv.ru>
2016-12-03 10:35:25 +0300

    [site] - main (react)

--- src/client/main.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom'
-import {Copyright} from './components/Copyright'
-
-ReactDOM.render(<Copyright props={{now: 'hi'}}/>, document.querySelector('.body-wrapper'))
-
-//<react-component name="Copyright" props="now" watch-depth="reference"/>
