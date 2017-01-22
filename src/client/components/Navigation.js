178d3c795597ee2205bba3fa2e6cf9d837943de8
Roman Timashev <roman@tmshv.ru>
2017-01-22 08:07:41 +0300

    Navigator eslinterd

--- src/client/components/Navigation.js
+++ src/client/components/Navigation.js
@@ -1,12 +1,12 @@
-import template from '../../templates/components/navigation.html';
+import template from '../../templates/components/navigation.html'
 
 export default function (app) {
-    app.component('navigation', {
-        bindings: {
-            menu: '<',
-            rightText: '@'
-        },
-        template: template,
-        controllerAs: '$'
-    });
+	app.component('navigation', {
+		bindings: {
+			menu: '<',
+			rightText: '@'
+		},
+		template: template,
+		controllerAs: '$'
+	})
 };
