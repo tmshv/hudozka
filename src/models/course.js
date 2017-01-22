2d0b6499570777d890e395da9c00bec8e4dd9e41
Roman Timashev <roman@tmshv.ru>
2017-01-23 00:59:26 +0300

    course model eslinted

--- src/models/course.js
+++ src/models/course.js
@@ -1,18 +1,18 @@
 export const courses = {
-	painting: "Живопись",
-	drawing: "Рисунок",
-	sculpting: "Скульптура",
-    sculpture: "Скульптура",
-	ceramic: "Керамика",
-    ceramics: "Керамика",
-	composition: "Композиция",
-	history: "История искусства",
-	cg: "Комп. графика"
-};
+	painting: 'Живопись',
+	drawing: 'Рисунок',
+	sculpting: 'Скульптура',
+	sculpture: 'Скульптура',
+	ceramic: 'Керамика',
+	ceramics: 'Керамика',
+	composition: 'Композиция',
+	history: 'История искусства',
+	cg: 'Комп. графика'
+}
 
 export function getCourseName(id) {
-    const course = Object
+	const course = Object
         .entries(courses)
-        .find(([key]) => key === id);
-    return course ? course[1] : null;
+        .find(([key]) => key === id)
+	return course ? course[1] : null
 }
