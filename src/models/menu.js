ba102fc9fc2c281ae6e9a42cbccd001d54c85242
Roman Timashev <roman@tmshv.ru>
2017-02-20 02:31:44 +0300

    menu eslinted

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,47 +1,47 @@
 export default [
-    {
-        "text": "Школа",
-        "url": "/",
-        "color": "blue"
-    },
-    {
-        "text": "События",
-        "url": "/events",
-        "color": "orange"
-    },
-    {
-        "text": "Расписание",
-        "url": "/schedule",
-        "color": "green"
-    },
-    {
-        "text": "Предметы",
-        "url": "/courses",
-        "color": "magenta"
-    },
-    {
-        "text": "Коллектив",
-        "url": "/collective",
-        "color": "pink"
-    },
-    {
-        "text": "Галерея",
-        "url": "/gallery",
-        "color": "red"
-    },
-    {
-        "text": "Награды",
-        "url": "/awards",
-        "color": "yellow"
-    },
-    {
-        "text": "Документы",
-        "url": "/documents",
-        "color": "yellow"
-    },
-    {
-        "text": "Сведения об образовательной организации",
-        "url": "/info",
-        "color": "gray"
-    }
-];
+	{
+		'text': 'Школа',
+		'url': '/',
+		'color': 'blue'
+	},
+	{
+		'text': 'События',
+		'url': '/events',
+		'color': 'orange'
+	},
+	{
+		'text': 'Расписание',
+		'url': '/schedule',
+		'color': 'green'
+	},
+	{
+		'text': 'Предметы',
+		'url': '/courses',
+		'color': 'magenta'
+	},
+	{
+		'text': 'Коллектив',
+		'url': '/collective',
+		'color': 'pink'
+	},
+	{
+		'text': 'Галерея',
+		'url': '/gallery',
+		'color': 'red'
+	},
+	{
+		'text': 'Награды',
+		'url': '/awards',
+		'color': 'yellow'
+	},
+	{
+		'text': 'Документы',
+		'url': '/documents',
+		'color': 'yellow'
+	},
+	{
+		'text': 'Сведения об образовательной организации',
+		'url': '/info',
+		'color': 'gray'
+	}
+]
