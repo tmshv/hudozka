82abbed156e937885295e7ebd2e9df8e0d25415f
Roman Timashev <tmshv@yandex-team.ru>
2017-10-16 03:25:11 +0300

    [www] HudozkaTitle no nbsp

--- src/components/HudozkaTitle.js
+++ src/components/HudozkaTitle.js
@@ -3,7 +3,7 @@ const React = require('react')
 const HudozkaTitle = () => (
 	<div className="HudozkaTitle">
 		<p>МБУДО</p>
-		<h1>Шлиссельбургская детская художественная&nbsp;школа</h1>
+		<h1>Шлиссельбургская детская художественная школа</h1>
 	</div>
 )
 
