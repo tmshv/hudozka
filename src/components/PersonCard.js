1d1b19e2e9a235fea94e0a635b27547fa4bbe1f5
Roman Timashev <tmshv@yandex-team.ru>
2017-10-28 19:22:58 +0300

    [www] person card clean

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -7,14 +7,6 @@ const Link = ({href, children}) => (
 	</a>
 )
 
-const Info = ({children}) => (
-	<div className="Article Article--cloud">
-		<div className="Article-Body">
-			{children}
-		</div>
-	</div>
-)
-
 module.exports = ({picture, name, url, profile}) => (
 	<div className="PersonCard">
 		<Link href={url}>
@@ -28,13 +20,6 @@ module.exports = ({picture, name, url, profile}) => (
 
 			<div className="PersonCard-Body">
 				{profile.position}
-
-				{/*<Info>*/}
-				{/*<p className="PersonCard-Position"><i><strong>{profile.position}</strong></i></p>*/}
-				{/*<p>{profile.status}</p>*/}
-				{/*<p>Образование: {profile.edu}</p>*/}
-				{/*<p>Диплом: <i>{profile.diploma}</i></p>*/}
-				{/*</Info>*/}
 			</div>
 		</Link>
 	</div>
