f174d297f299dab00e390092325690eb40676739
Roman Timashev <tmshv@yandex-team.ru>
2017-10-28 17:26:18 +0300

    [www] renderAlbum

--- src/routes/albums.js
+++ src/routes/albums.js
@@ -1,36 +1,13 @@
-const React = require('react')
-const Article = require('../components/Article')
-const Album = require('../core/Album')
-const {render} = require('../lib/render')
-const getPathWithNoTrailingSlash = require('../lib/url').getPathWithNoTrailingSlash
+const {renderAlbum} = require('../render/albums')
 const {get} = require('koa-route')
-
-function getMeta(album) {
-	return {
-		title: album.title,
-		description: album.title,
-	}
-}
 
 function getAlbum() {
 	return get('/album/:id', async (ctx, id) => {
-		const path = getPathWithNoTrailingSlash(ctx.path)
-		const album = await Album.findById(id)
+		const body = await renderAlbum(id)
 
-		if (album) {
-			const Component = (
-				<div className="content content_thin">
-					<Article
-						title={album.title}
-						date={album.date}
-						data={album.post}
-						shareable={true}
-					/>
-				</div>
-			)
-
+		if (body) {
 			ctx.type = 'text/html'
-			ctx.body = await render(path, Component, getMeta(album), {commentsEnabled: true, menuPadding: true})
+			ctx.body = body
 		} else {
 			ctx.status = 404
 		}
