7e1a5c7af0ba00f92510a31ae6ceaf6e56b21a45
Roman Timashev <roman@tmshv.ru>
2019-08-01 17:39:08 +0300

    api articles urls

---
+++ pages/api/articles/urls.js
@@ -0,0 +1,20 @@
+import { connect } from '../../../src/core/db'
+import Article from '../../../src/core/Article'
+
+export default async (req, res) => {
+    await connect()
+
+    const data = await Article.find({})
+
+    if (data) {
+        const items = data.map(x => `/article/${x.id}`)
+        res.json({
+            items
+        })
+    } else {
+        res.status(404)
+        res.json({
+            error: `Article ${id} not found`
+        })
+    }
+}
