ecd6efd4fd172cbb0485cc73bfdcbd9f0a7670ac
Roman Timashev <roman@tmshv.ru>
2019-08-02 16:23:41 +0300

    teacher title

--- pages/teacher/[person].jsx
+++ pages/teacher/[person].jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import Head from 'next/head'
 import App from '../../src/components/App'
 import Page from '../../src/components/Page'
 import menuModel from '../../src/models/menu'
@@ -11,6 +12,9 @@ const Index = (props) => (
         showAuthor={true}
         menuPadding={true}
     >
+        <Head>
+            <title>{props.title}</title>
+        </Head>
         <div className="content content_thin">
             <Page shareable={true}>
                 {props.person.post}
@@ -25,10 +29,12 @@ Index.getInitialProps = async (ctx) => {
     const apiUrl = `http://localhost:3000/api/persons/${id}`
     const res = await axios.get(apiUrl)
     const person = res.data
+    const name = person.name || []
 
     return {
         pageUrl,
         person,
+        title: name.join(' ')
     }
 }
 
