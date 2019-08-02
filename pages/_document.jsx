049c0f4aade4c96e8c23e040aef076012332bcba
Roman Timashev <roman@tmshv.ru>
2019-08-02 16:34:45 +0300

    populating head with meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,22 @@ export default class MyDocument extends Document {
                 <Head>
                     <style>{`body { margin: 0 } /* custom! */`}</style>
 
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width, maximum-scale=1.0" />
+                    <meta name="referrer" content="unsafe-url" />
+
+                    <meta name="google-site-verification" content="8BxOTw-Q02DS2KAdcGcnIMwiPZ58DVB18XpOfRibCcg" />
+                    <meta name="google-site-verification" content="A-TEemUw4hHpULUDCd6xhxfHnyRbgKtss90JGKXnSpw" />
+                    <meta name='yandex-verification' content='643c7dac144b9dac' />
+                    <meta name="msvalidate.01" content="E878301FE3E3F12D233A6B1156889601" />
+
+                    {/* <link
+                        rel="alternate"
+                        type="application/rss+xml"
+                        title="Шлиссельбургская Детская Художественная Школа"
+                        href="/feed"
+                    /> */}
+
                     <script src='/static/likely.js'></script>
                     <link href='/static/likely.css' rel='stylesheet' />
                 </Head>
