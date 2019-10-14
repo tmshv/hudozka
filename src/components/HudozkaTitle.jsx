274e1e7fc06ecdf19a83f0632c8c4f8c28cfe461
Roman Timashev <roman@tmshv.ru>
2019-10-14 22:33:02 +0300

    hudozka title style moved to styled jsx

--- src/components/HudozkaTitle.jsx
+++ src/components/HudozkaTitle.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
 export const HudozkaTitle = () => (
-	<div className="HudozkaTitle">
-		<p>МБУДО</p>
-		<h1>Шлиссельбургская детская художественная школа</h1>
-	</div>
+    <div>
+        <style jsx>{`
+            div {
+                --padding: var(--double-margin) 0;
+
+                padding: var(--padding);
+                text-align: center;
+            }
+
+            p {
+                color: var(--text-color-second);
+                margin-top: var(--single-margin);
+                font-size: var(--font-size-second);
+            }
+
+            h1 {
+                margin-top: var(--single-margin);
+            }
+
+            @media (max-width: var(--mobile-width)) {
+                div {
+                    --padding: 0 0 var(--double-margin);
+                }
+            }
+        `}</style>
+
+        <p>МБУДО</p>
+        <h1>Шлиссельбургская детская художественная школа</h1>
+    </div>
 )
