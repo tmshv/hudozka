5d672842f364435c85ff3bc9d9c3206240034b15
Roman Timashev <roman@tmshv.ru>
2019-10-15 02:46:41 +0300

    mobile style variables

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+
+class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props
+
+        return (
+            <Container>
+                <style global jsx>{`
+                    @media (max-width: 31.25em) {
+                        :root {
+                            --smaller-font-size: 0.9rem;
+                            --normal-font-size: 1rem;
+                            --bigger-font-size: 1.1rem;
+                            --big-font-size: 1.3rem;
+                            --biggest-font-size: 2rem;
+
+                            --font-size-h1: 1.75rem;
+                            --font-size-h2: 1.5rem;
+                            --font-size-h3: 1.25rem;
+                        }
+                    }
+
+                    #__next {
+                        height: 100%;
+                    }
+                `}</style>
+
+                <Component {...pageProps} />
+            </Container>
+        )
+    }
+}
+
+export default MyApp
