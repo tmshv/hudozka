8c2b17150bc61a95dfa7ca37edc648c37e4b863f
Roman Timashev <roman@tmshv.ru>
2019-10-21 15:24:14 +0300

    api articles typing fix

--- pages/api/articles/index.ts
+++ pages/api/articles/index.ts
@@ -1,10 +1,15 @@
 import { findArticles } from '../../../src/server/articles'
 import { withMiddleware } from '../../../src/middlewares/withMiddleware'
 
+const int = (value: string | string[]) => parseInt(Array.isArray(value)
+    ? value[0]
+    : value
+)
+
 export default withMiddleware(async (req, res) => {
     const { query } = req
-    const page = parseInt(query.page)
-    const pageSize = parseInt(query.pageSize)
+    const page = int(query.page)
+    const pageSize = int(query.pageSize)
 
     const data = await findArticles(page, pageSize)
 
