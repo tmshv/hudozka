68b6d36e0e188c83646bdf4a684be415808a6c36
Roman Timashev <roman@tmshv.ru>
2020-02-12 23:43:25 +0300

    collective page updated + ts

--- pages/collective.tsx
+++ pages/collective.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
 import Head from 'next/head'
 import { get } from 'lodash'
-import { App } from '../src/components/App'
-import { Meta } from '../src/components/Meta'
-import { CollectiveImage } from '../src/components/CollectiveImage'
-import { PersonCard } from '../src/components/PersonCard'
-import { CardList } from '../src/components/CardList'
-import menuModel from '../src/models/menu'
-import { buildMenu } from '../src/lib/menu'
-import { meta } from '../src/lib/meta'
-import { createApiUrl, requestGet, wrapInitialProps } from '../src/next-lib'
+import { App } from 'src/components/App'
+import { Meta } from 'src/components/Meta'
+import { CollectiveImage } from 'src/components/CollectiveImage'
+import { PersonCard } from 'src/components/PersonCard'
+import { CardList } from 'src/components/CardList'
+import menuModel from 'src/models/menu'
+import { buildMenu } from 'src/lib/menu'
+import { meta } from 'src/lib/meta'
+import { IMeta } from 'src/types'
+import { createApiUrl, requestGet, wrapInitialProps, IResponseItems } from 'src/next-lib'
+import { NextPage } from 'next'
 
-const Page = (props) => (
+interface IPerson {
+
+}
+
+type Props = {
+    pageUrl: string
+    title: string
+    image: string
+    meta: IMeta
+    persons: IPerson[]
+}
+
+const Page: NextPage<Props> = props => (
     <App
         menu={buildMenu(props.pageUrl, menuModel)}
         showAuthor={true}
@@ -50,7 +63,7 @@ const Page = (props) => (
 Page.getInitialProps = wrapInitialProps(async (ctx) => {
     const pageUrl = ctx.req.url
 
-    const res = await requestGet(createApiUrl(ctx.req, '/api/persons'), {})
+    const res = await requestGet<IResponseItems<IPerson>>(createApiUrl(ctx.req, '/api/persons'), { items: [] })
     const persons = res.items || []
     const title = 'Преподаватели Шлиссельбургской ДХШ'
     const imageFile = 'Images/HudozkaCollective2017.jpg'
