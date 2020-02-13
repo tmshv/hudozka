e45c69dee07f5baa5ea0d6cbab0cf40d7f17807a
Roman Timashev <roman@tmshv.ru>
2020-02-13 22:37:29 +0300

    styles refactoring: move root to css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'src/style.css'
 import App from 'next/app'
 
 export default class MyApp extends App {
@@ -5,36 +6,7 @@ export default class MyApp extends App {
         const { Component, pageProps } = this.props
 
         return (
-            <>
-                <style global jsx>{`
-                    @media (max-width: 31.25em) {
-                        :root {
-                            --smaller-font-size: 0.9rem;
-                            --normal-font-size: 1rem;
-                            --bigger-font-size: 1.1rem;
-                            --big-font-size: 1.3rem;
-                            --biggest-font-size: 2rem;
-
-                            --font-size-h1: 1.5rem;
-                            --font-size-h2: 1.5rem;
-                            --font-size-h3: 1.25rem;
-
-                            --content-thin: 90%;
-                            --content-wide: 90%;
-                        }
-                    }
-
-                    .noscroll { 
-                        overflow: hidden;
-                    }
-
-                    #__next {
-                        height: 100%;
-                    }
-                `}</style>
-
-                <Component {...pageProps} />
-            </>
+            <Component {...pageProps} />
         )
     }
 }
