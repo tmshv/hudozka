c0dd5cf6aed9acb4bdf011a5b7862e8c4e756ef3
Roman Timashev <roman@tmshv.ru>
2020-02-18 14:26:09 +0300

    move footer props to config

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export default {
+    address: "г. Шлиссельбург ул. 18 января д. 3",
+    telephone: "+7 (81362) 76-312",
+    email: "hudozka@gmail.com",
+}
---
+++ src/context/ConfigContext.ts
@@ -0,0 +1,4 @@
+import { createContext } from 'react'
+import config from 'src/config'
+
+export const ConfigContext = createContext(config)
