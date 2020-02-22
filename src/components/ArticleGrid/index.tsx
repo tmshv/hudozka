11d628fd50ec4e92aea07263d8a4f4b3ed20685f
Roman Timashev <roman@tmshv.ru>
2020-02-22 03:28:04 +0300

    article grid missing home page fix

--- src/components/ArticleGrid/index.tsx
+++ src/components/ArticleGrid/index.tsx
@@ -12,56 +12,64 @@ export type ArticleGridProps = {
     prevPage: number | null
 }
 
-export const ArticleGrid: React.FC<ArticleGridProps> = ({ articles, nextPage, prevPage, ...props }) => (
-    <>
-        <CardGrid
-            style={{
-                marginBottom: 'var(--size-m)',
-            }}
-        >
-            {articles.map((article, i) => {
-                const gridColumn = article.featured ? 'span 2' : 'auto'
-                const layout: CardLayout = article.featured ? 'featured' : 'simple'
-                const srcSet = article.preview.set.map(({ url, density }) => `${url} ${density}x`).join(' ')
+export const ArticleGrid: React.FC<ArticleGridProps> = ({ articles, nextPage, prevPage, ...props }) => {
+    const nextHref = nextPage ? `/articles/${nextPage}` : null
+    let prevHref = prevPage ? `/articles/${prevPage}` : null
+    if (prevPage === 1) {
+        prevHref = '/'
+    }
 
-                const content = !article.featured
-                    ? (
-                        <Block
-                            direction={'vertical'}
+    return (
+        <>
+            <CardGrid
+                style={{
+                    marginBottom: 'var(--size-m)',
+                }}
+            >
+                {articles.map((article, i) => {
+                    const gridColumn = article.featured ? 'span 2' : 'auto'
+                    const layout: CardLayout = article.featured ? 'featured' : 'simple'
+                    const srcSet = article.preview.set.map(({ url, density }) => `${url} ${density}x`).join(' ')
+
+                    const content = !article.featured
+                        ? (
+                            <Block
+                                direction={'vertical'}
+                            >
+                                {article.title}
+                                <Spacer />
+                                <Date style={{
+                                    marginTop: 'var(--size-m)',
+                                }}>{article.date}</Date>
+                            </Block>
+                        ) : (
+                            article.title
+                        )
+
+                    return (
+                        <Card
+                            key={i}
+                            href={article.url}
+                            img={{
+                                alt: article.title,
+                                src: article.preview.src,
+                                srcSet,
+                            }}
+                            layout={layout}
+                            style={{
+                                gridColumn,
+                            }}
                         >
-                            {article.title}
-                            <Spacer />
-                            <Date style={{
-                                marginTop: 'var(--size-m)',
-                            }}>{article.date}</Date>
-                        </Block>
-                    ) : (
-                        article.title
+                            {content}
+                        </Card>
                     )
+                })}
+            </CardGrid>
 
-                return (
-                    <Card
-                        key={i}
-                        href={article.url}
-                        img={{
-                            alt: article.title,
-                            src: article.preview.src,
-                            srcSet,
-                        }}
-                        layout={layout}
-                        style={{
-                            gridColumn,
-                        }}
-                    >
-                        {content}
-                    </Card>
-                )
-            })}
-        </CardGrid>
-
-        <Pager
-            nextHref={nextPage ? `/articles/${nextPage}` : null}
-            prevHref={prevPage ? `/articles/${prevPage}` : null}
-        />
-    </>
-)
+            <Pager
+                nextHref={nextHref}
+                prevHref={prevHref}
+            />
+        </>
+    )
+}
