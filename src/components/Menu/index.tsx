be9f07bdb79f93b054199f572856f23d82d51a5e
Roman Timashev <roman@tmshv.ru>
2020-02-22 03:40:14 +0300

    Menu component updated

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react'
-import cx from 'classnames'
-import { getPathWithNoTrailingSlash } from '../../lib/url'
-import { MenuItem } from './MenuItem'
+import './styles.css'
 
-const itemUrl = (url: string) => getPathWithNoTrailingSlash(url)
+import cx from 'classnames'
+import { ActiveLink } from './ActiveLink'
 
 export interface IMenuProps {
     layout: 'desktop' | 'mobile'
@@ -12,113 +10,20 @@ export interface IMenuProps {
 
 export const Menu: React.FC<IMenuProps> = props => (
     <div className={cx('menu', props.layout)}>
-        <style jsx>{`
-            .menu {
-                display: flex;
-                align-items: flex-start;
-
-                list-style: none;
-            }
-
-            div.desktop {
-                flex-direction: row;
-                flex-wrap: wrap;
-            }
-
-            div.mobile {
-                flex-direction: column;
-            }
-            
-            li{
-                position: relative;
-                border-top: var(--menu-colored-mark-thickness) solid rgba(0, 0, 0, 0);
-            }
-
-            li.desktop {
-                padding: 0.3em 0.2em 0;
-                margin-right: 1em;
-
-                font-size: var(--normal-font-size);
-            }
-
-            li.mobile {
-                padding: 0.3em 0.5em;
-                margin-bottom: var(--half-margin);
-
-                font-size: 1.25em;
-                color: white;
-            }
-
-            li:last-child {
-                margin-right: 0;
-            }
-
-            li.selected {
-                font-weight: bold;
-            }
-
-            li.desktop.selected.blue {
-                border-top-color: var(--blue-color);
-            }
-
-            li.desktop.selected.orange {
-                border-top-color: var(--orange-color);
-            }
-
-            li.desktop.selected.green {
-                border-top-color: var(--green-color);
-            }
-
-            li.desktop.selected.yellow {
-                border-top-color: var(--yellow-color);
-            }
-
-            li.desktop.selected.pink {
-                border-top-color: var(--pink-color);
-            }
-
-            li.desktop.selected.red {
-                border-top-color: var(--red-color);
-            }
-
-            li.mobile.selected.blue {
-                background-color: var(--blue-color);
-            }
-
-            li.mobile.selected.orange {
-                background-color: var(--orange-color);
-            }
-
-            li.mobile.selected.green {
-                background-color: var(--green-color);
-            }
-
-            li.mobile.selected.yellow {
-                background-color: var(--yellow-color);
-            }
-
-            li.mobile.selected.pink {
-                background-color: var(--pink-color);
-            }
-
-            li.mobile.selected.red {
-                background-color: var(--red-color);
-            }
-        `}</style>
-
         {props.items.map((item, index) => (
             <li key={index}
-                className={cx(
-                    props.layout,
-                    item.color, {
-                    selected: item.highlighted,
+                className={cx('menuItem', props.layout, {
+                    selected: item.highlighted
                 })}
             >
-                <MenuItem
-                    url={itemUrl(item.url)}
-                    text={item.text}
-                    active={item.active}
-                />
+                <ActiveLink
+                    href={!item.active ? item.url : null}
+                    activeStyle={{
+                        fontWeight: 'bold',
+                    }}
+                >
+                    {item.text}
+                </ActiveLink>
             </li>
         ))}
     </div>
