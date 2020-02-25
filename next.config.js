c943ffc8140d0188400f1e8f99cd2e25ce393fae
Roman Timashev <roman@tmshv.ru>
2020-02-25 18:30:31 +0300

    articles pages size changed to 24

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withSass = require('@zeit/next-sass')
 module.exports = withCss(withSass({
     exportTrailingSlash: true,
     env: {
-        APP_ARTICLES_PAGE_SIZE: 12,
+        APP_ARTICLES_PAGE_SIZE: 24,
     },
     webpack(config) {
         config.resolve.modules.push(__dirname)
