a01888f15576aee281492f0da6f6e45f7c65ccd3
Roman Timashev <roman@tmshv.ru>
2020-02-27 16:23:49 +0300

    comments removed

--- src/components/Comments.js
+++
@@ -1,9 +0,0 @@
-const React = require('react')
-
-const Comments = ({contentClass = 'content_thin'}) => (
-	<div className={`content ${contentClass}`}>
-		<div id="disqus_thread"/>
-	</div>
-)
-
-module.exports = Comments
