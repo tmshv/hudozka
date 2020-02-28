3fa1763913ccd443869643c665d9e6b25d74f00e
Roman Timashev <roman@tmshv.ru>
2020-02-29 01:57:43 +0300

    drop schedule from app

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -11,7 +11,6 @@ const api = axios.create({
 async function getMenuUrls(frequency = 'daily') {
     const menu = [
         '/',
-        '/schedule',
         '/collective',
     ]
     return menu
