830a193333a74540d7f405bd40d58caa800484b2
Roman Timashev <roman@tmshv.ru>
2020-03-04 15:41:28 +0300

    article update

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 import { Share } from '../Share'
 import { Html } from '../Html'
 import { PageMeta } from '../PageMeta'
@@ -11,19 +13,12 @@ export type PageProps = {
 }
 
 export const Page: React.FC<PageProps> = props => (
-    <div className={'Article'} style={props.style}>
-        <style jsx>{`
-            hr {
-                border: 1px solid black;
-                margin-top: var(--size-m);
-                margin-bottom: var(--size-m);
-            }
-        `}</style>
-        <div className={'Article-Body'}>
-            <Html
-                html={props.children}
-            />
-        </div>
+    <div className={'page'} style={props.style}>
+        <Html
+            as={'article'}
+            className={'article'}
+            html={props.children}
+        />
 
         <hr />
         <Share />
