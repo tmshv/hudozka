1a006f5b3f766d4d71dee27689891f1cd23821b9
Roman Timashev <roman@tmshv.ru>
2020-03-19 15:02:38 +0300

    document scss moved to file css

--- src/components/File/index.tsx
+++ src/components/File/index.tsx
@@ -1,24 +1,26 @@
+import './styles.css'
+
 import { FileDefinition } from 'src/types'
 import { ext, size } from 'src/lib/file'
 import { titleCase } from 'src/lib/string'
 
 export const File: React.SFC<FileDefinition> = props => (
-    <div className="document">
+    <div className={'file'}>
         <h1>{titleCase(props.name)}</h1>
 
         {!props.cover?.src ? null : (
-            <div className="document__image">
+            <div className={'file__image'}>
                 <a href={props.file.src} target="_blank" className="invisible">
                     <img src={props.cover.src} alt={props.name} />
                 </a>
             </div>
         )}
 
-        <div className="document__file">
+        <div className={'file__file'}>
             <span>{titleCase(props.name)}</span>
         </div>
 
-        <div className="document__file-info">
+        <div className={'file__file-info'}>
             <a href={props.file.src} target="_blank">{ext(props.file.name)} ({size(props.file.size)})</a>
         </div>
     </div>
