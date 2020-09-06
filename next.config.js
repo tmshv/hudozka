73aa95b6d38d932d13af3c7037cbd2a58f1c421a
Roman Timashev <roman@tmshv.ru>
2020-09-07 00:09:46 +0300

    tsconfig absolute import improved

--- next.config.js
+++ next.config.js
@@ -4,8 +4,4 @@ module.exports = {
         APP_ARTICLES_PAGE_SIZE: 24,
         APP_CARD_DEFAULT_IMAGE: 'https://art.shlisselburg.org/static/img/main.jpg',
     },
-    webpack(config) {
-        config.resolve.modules.push(__dirname)
-        return config;
-    },
 }
