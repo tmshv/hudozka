26a6eb3642f5573543e61b6ace23d6fee4f5ca25
Roman Timashev <roman@tmshv.ru>
2020-09-07 00:09:01 +0300

    trailing slash option renamed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    exportTrailingSlash: true,
+    trailingSlash: true,
     env: {
         APP_ARTICLES_PAGE_SIZE: 24,
         APP_CARD_DEFAULT_IMAGE: 'https://art.shlisselburg.org/static/img/main.jpg',
