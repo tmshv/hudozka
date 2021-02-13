00197d4f9ac2871fafc76c8783ba7a46af674a92
Roman Timashev <roman@tmshv.ru>
2021-02-13 14:33:21 +0300

    add image config to next config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ module.exports = {
         APP_ARTICLES_PAGE_SIZE: 24,
         APP_CARD_DEFAULT_IMAGE: 'https://art.shlisselburg.org/static/img/main.jpg',
     },
+    images: {
+        domains: [
+            'hudozkacdn.tmshv.com',
+            'images.weserv.nl',
+        ],
+    },
 }
