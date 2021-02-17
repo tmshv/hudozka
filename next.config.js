a890b1bb757149ebfd325bbff0adece7bfcf8ba5
Roman Timashev <roman@tmshv.ru>
2021-02-17 15:17:46 +0300

    no trailing slash

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    trailingSlash: true,
+    trailingSlash: false,
     env: {
         APP_ARTICLES_PAGE_SIZE: 24,
         APP_CARD_DEFAULT_IMAGE: 'https://art.shlisselburg.org/static/img/main.jpg',
