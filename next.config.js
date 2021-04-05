63ffaf6b2003b112da1f2a4012f0429009c5007d
Roman Timashev <roman@tmshv.ru>
2021-04-05 17:08:45 +0300

    next env refactoring

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     trailingSlash: false,
-    env: {
-        APP_ARTICLES_PAGE_SIZE: 24,
-        APP_CARD_DEFAULT_IMAGE: 'https://art.shlisselburg.org/static/img/main.jpg',
-    },
     images: {
         domains: [
             'hudozkacdn.tmshv.com',
