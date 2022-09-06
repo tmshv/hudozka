c3b809cc72b150694189bb69920a7f9045808723
Roman Timashev <roman@tmshv.ru>
2022-09-06 13:29:33 +0300

    lib image typing fix

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -6,9 +6,6 @@ export type ResizeOptions = {
 
 export function getResizedUrl(src: string, options: ResizeOptions): string {
     const n = options.n ?? 1
-    if (!src || src === '') {
-        return null
-    }
 
     return `https://images.weserv.nl/?url=${src}&w=${options.width}&h=${options.height}&n=${n}`
 }
