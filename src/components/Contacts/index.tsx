ad96a7add234c51dd073bdde2eacd8963469a86e
Roman Timashev <roman@tmshv.ru>
2022-09-06 14:10:35 +0300

    Contacts update

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,25 +1,22 @@
+import s from './contacts.module.css'
+
 import { useContext } from 'react'
 import { ConfigContext } from 'src/context/ConfigContext'
+import Link from 'next/link'
 
 export const Contacts: React.FC = props => {
     const { contacts } = useContext(ConfigContext)
 
     return (
         <>
-            <style jsx>{`
-                strong {
-                    margin-bottom: var(--size-m);
-                }
-
-                p {
-                    margin-bottom: var(--size-s);
-                }
-            `}</style>
-
-            <strong>Контактная информация</strong>
+            <strong className={s.strong}>Контактная информация</strong>
 
             {contacts.map((x, i) => (
-                <p key={i}><a href={x.value}>{x.title}</a></p>
+                <p key={i} className={s.link}>
+                    <Link href={x.value}>
+                        <a >{x.title}</a>
+                    </Link>
+                </p>
             ))}
         </>
     )
