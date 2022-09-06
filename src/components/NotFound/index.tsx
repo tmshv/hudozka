3138022619eb6c493e8e3eec0a8b707de0028e77
Roman Timashev <roman@tmshv.ru>
2022-09-06 14:01:28 +0300

    NotFound component no jsx

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,31 +1,9 @@
+import s from './error.module.css'
+
 export const NotFound: React.FC = () => (
-    <section> 
-        <style jsx>{`
-            section {
-                margin-top: var(--size-l);
-                margin-bottom: var(--size-l);
-
-                text-align: center;
-                height: 100%;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-            }
-
-            h1 {
-                font-size: 16pt;
-                font-weight: normal;
-            }
-
-            img {
-                margin-top: var(--size-m);
-                margin-bottom: var(--size-m);
-                max-width: 400px;
-            }
-        `}</style>
-        
+    <section className={s.section}>
         <div>
-            <h1>Страница не найдена.</h1>
+            <h1>Страница не найдена</h1>
             <img src="/static/graphics/skull.jpg" />
         </div>
 
