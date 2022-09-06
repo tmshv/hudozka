73a9635129d255ab0ce6f98cee5ebf9b8cbff6cd
Roman Timashev <roman@tmshv.ru>
2022-09-06 13:06:04 +0300

    rm unsed splibBy function

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,13 +1,3 @@
-export function splitBy(fn) {
-    return items => {
-        return items.reduce((acc, item) => {
-            const key = fn(item)
-            const items = acc.get(key) || []
-
-            return acc.set(key, [...items, item])
-        }, new Map())
-    }
-}
 
 export function insertBetween<T, I = T>(items: T[], fn: (i: number) => I): Array<T | I> {
     return items.reduce<Array<T | I>>((acc, x, i) => {
