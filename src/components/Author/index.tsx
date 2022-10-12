8d67524526312793e6557bcfd17f4b09929d2246
Roman Timashev <roman@tmshv.ru>
2022-10-12 16:27:27 +0600

    author comopnent fix

--- src/components/Author/index.tsx
+++ src/components/Author/index.tsx
@@ -1,6 +1,6 @@
 export const Author: React.FC = () => (
     <span>
-        Разработка и поддержка сайта —
+        Разработка и поддержка — {" "}
         <a href="http://tmshv.ru?utm_source=artshburg&utm_medium=developer&utm_campaign=tmshv_ru">
             www.tmshv.ru
         </a>
