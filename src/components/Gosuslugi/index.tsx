ac3bbb825dec7b3f79e3cb681340016b2225b44d
Roman Timashev <roman@tmshv.ru>
2023-08-16 17:23:15 +0300

    fix gosuslugi

--- src/components/Gosuslugi/index.tsx
+++ src/components/Gosuslugi/index.tsx
@@ -1,14 +1,23 @@
 import { useEffect } from "react"
 import { getCode } from "./lib"
+import InnerHTML from "dangerously-set-html-content"
 
 export type GosuslugiProps = {}
 
 export const Gosuslugi: React.FC<GosuslugiProps> = () => {
     useEffect(() => {
-        window.dispatchEvent(new Event("resize"))
-    })
+        const id = setTimeout(() => {
+            // This call taken from getCode <script>Widget("https://pos.gosuslugi.ru/form", 312519)</script>
+            (window as any).Widget("https://pos.gosuslugi.ru/form", 312519)
+        }, 100)
+
+        return () => {
+            clearTimeout(id)
+        }
+    }, [])
 
     return (
-        <div dangerouslySetInnerHTML={{ __html: getCode() }} />
+        <InnerHTML html={getCode()} />
     )
 }
+
