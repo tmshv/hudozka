320768b6dbc2e337178bd65bf3eaf7e2ac5a0a7f
Roman Timashev <roman@tmshv.ru>
2023-08-16 20:09:08 +0300

    add repeat for gosuslugi

--- src/components/Gosuslugi/index.tsx
+++ src/components/Gosuslugi/index.tsx
@@ -2,17 +2,41 @@ import { useEffect } from "react"
 import { getCode } from "./lib"
 import InnerHTML from "dangerously-set-html-content"
 
+async function sleep(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    })
+}
+
+async function repeat(delay: number, times: number, fn: () => void): Promise<void> {
+    try {
+        fn()
+    } catch (error) {
+        if (times === 0) {
+            throw error
+        }
+
+        await sleep(delay)
+        return repeat(delay, times - 1, fn)
+    }
+}
+
 export type GosuslugiProps = {}
 
 export const Gosuslugi: React.FC<GosuslugiProps> = () => {
     useEffect(() => {
-        const id = setTimeout(() => {
+        let mount = true
+        repeat(1000, 50, () => {
+            if (!mount) {
+                return
+            }
+
             // This call taken from getCode <script>Widget("https://pos.gosuslugi.ru/form", 312519)</script>
             (window as any).Widget("https://pos.gosuslugi.ru/form", 312519)
-        }, 100)
+        })
 
         return () => {
-            clearTimeout(id)
+            mount = false
         }
     }, [])
 
