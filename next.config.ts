141abfb5eabe6f3058694160385d9acb34bc7c4b
Roman Timashev <roman@tmshv.ru>
2026-02-08 22:24:15 +0700

    git upd next conf

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next"
 const config: NextConfig = {
     output: "standalone",
     trailingSlash: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         remotePatterns: [
             { hostname: "hudozkacdn.tmshv.com" },
