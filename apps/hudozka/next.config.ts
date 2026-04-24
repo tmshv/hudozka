305eca14dfce34b40ac6e98133f1f2e9eee740d8
Roman Timashev <roman@tmshv.ru>
2026-04-24 23:14:36 +0300

    add dockerfile path

--- apps/hudozka/next.config.ts
+++ apps/hudozka/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next"
+import path from "node:path"
 
 const config: NextConfig = {
     output: "standalone",
+    outputFileTracingRoot: path.join(process.cwd(), "../../"),
     trailingSlash: false,
     images: {
         remotePatterns: [
